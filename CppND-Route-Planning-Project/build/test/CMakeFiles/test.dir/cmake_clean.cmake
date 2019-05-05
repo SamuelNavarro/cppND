cbf9fbc8d8b4faf71bc365ebd9b39b35de45d93b
Samuel <samuelnavarro@datomico.com>
2019-05-05 17:19:14 -0500

    Remove build dir

--- CppND-Route-Planning-Project/build/test/CMakeFiles/test.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../bin/test.pdb"
-  "../../bin/test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuel/Documents/cppND/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
-# Build directory: /home/samuel/Documents/cppND/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
