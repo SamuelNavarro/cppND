b92410f701502c602a6956c6bfd7e08145a9af10
Samuel <samuelnavarro@datomico.com>
2019-05-11 22:16:59 -0500

    Update Advanced OOP

--- OOP/AdvancedOOP/friend_class.cpp
+++ OOP/AdvancedOOP/friend_class.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 
 
-class Rectangle;
 
 
 class Square {
 	friend class Rectangle;
 	private:
-		int slide;
+		int side;
 
 	public:
-		Square(int _slide) : slide(_slide){};
+		Square(int _side) : side(_side){};
 };
+
+
+class Rectangle {
+	friend class Square;
+	private:
+		int width, height;
+	public:
+		int area(){ return width * height; }
+		void convert(Square a);
+};
+
+
+void Rectangle::convert(Square a){
+	Rectangle::height = a.side;	
+	Rectangle::width = a.side;
+}
 
 
 int main(){
 
+	Square square(10);
+	Rectangle rec;
+	rec.convert(square);
+	std::cout << rec.area() << std::endl;
 
-	return 0;
 }
