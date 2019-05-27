90bd2618735928d688337ad1e1373dffd3549029
Samuel <samuelnavarro@datomico.com>
2019-05-27 14:11:30 -0500

    Done - But bad layout and constantly returning Non-existing PID

--- SystemMonitor/Process.h
+++ SystemMonitor/Process.h
@@ -50,15 +50,15 @@ string Process::getProcess()
     this->up_time = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   "
+    return (this->pid + "       "
                     + this->user
-                    + "   "
+                    + "       "
                     + this->mem.substr(0,5)
-                    + "     "
+                    + "       "
                     + this->cpu.substr(0,5)
-                    + "     "
+                    + "       "
                     + this->up_time.substr(0,5)
-                    + "    "
+                    + "       "
                     + this->cmd.substr(0,30)
                     + "...");
 }
--- SystemMonitor/ProcessContainer.h
+++ SystemMonitor/ProcessContainer.h
@@ -2,7 +2,6 @@
 #include <vector>
 
 
-
 class ProcessContainer{
 
 private:
